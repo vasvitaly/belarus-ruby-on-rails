77d8945fd78a09b066dd27acabae887a9aa9834c
Aliaksei Maridashvili <amarydashvili@yahoo.ca>
2011-09-12 16:20:42 +0300

    Custom news is start page now

--- config/routes.rb
+++ config/routes.rb
@@ -55,7 +55,7 @@ BelarusRubyOnRails::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'welcome#index'
+  root :to => 'custom_news#index'
 
   # See how all your routes lay out with "rake routes"
 
