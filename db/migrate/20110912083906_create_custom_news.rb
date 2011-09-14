fd4564382e18de9533b048df534311c13bb07528
melnikaite <melnikaite.melnikaite@gmail.com>
2011-09-14 13:02:41 +0300

    activerecord creates primary key by default

--- db/migrate/20110912083906_create_custom_news.rb
+++ db/migrate/20110912083906_create_custom_news.rb
@@ -1,7 +1,6 @@
 class CreateCustomNews < ActiveRecord::Migration
   def up
     create_table :custom_news do |t|
-      t.primary_key :id
       t.string :title
       t.text :content
       t.integer :status
