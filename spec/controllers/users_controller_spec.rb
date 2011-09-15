137a2d0fcc43c5c1502c36cd798780ca636ab34b
Denis Sheleh <den.sheleh@gmail.com>
2011-09-15 10:52:41 +0300

    Fix indent in user_controller_spec.rb

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -9,15 +9,15 @@ describe UsersController do
 
   describe "GET 'show'" do
     
-    it "should be successful" do
-      get :show, :id => @user.id
-    response.should be_success
-    end
-    
-    it "should find the right user" do
-      get :show, :id => @user.id
-      assigns(:user).should == @user
-    end
+  it "should be successful" do
+    get :show, :id => @user.id
+  response.should be_success
+  end
+
+  it "should find the right user" do
+    get :show, :id => @user.id
+    assigns(:user).should == @user
+  end
     
   end
 
