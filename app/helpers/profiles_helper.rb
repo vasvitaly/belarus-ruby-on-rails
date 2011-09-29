238d51e1c3f81034f45559cfd67f4617f0ac244f
Denis Sheleh <den.sheleh@gmail.com>
2011-09-29 10:22:55 +0300

    Fix: link to binded FB page
    Change-Id: If55d9a025357f37edac33948370d65d03085338a
    Task: 49
    Reviewed-on: http://gerrit.altoros.corp/44
    Tested-by: Denis Sheleh <den.sheleh@gmail.com>
    Reviewed-by: Sergey Avseyev <sergey.avseyev@gmail.com>

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -1,7 +1,7 @@
 module ProfilesHelper
   def provider_link(provider, displayed_user_id)
     if provider.profile_link
-      link = link_to provider.profile_link
+      link = link_to(provider.profile_link, provider.profile_link, :target => '_blank')
     elsif current_user.id == displayed_user_id
       link = link_to(I18n.t('omniauth.bind_with', :provider_name => provider.printable_name),
                   user_omniauth_authorize_path(provider.provider_name))
