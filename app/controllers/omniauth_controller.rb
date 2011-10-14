4796124f0e6467f50544c8d47c729ac1f9ba4d3c
Eugene Melnikov <melnikaite.melnikaite@gmail.com>
2011-10-14 21:20:02 +0300

    Avoid redirect to login/registration page
    Added not redirectable urls to avoid errors like "you are already
    logged in".
    
    Change-Id: I385b002738eb0c0811013887cdeeefaa41ce02e0
    Reviewed-on: http://gerrit.altoros.corp/101
    Tested-by: Eugene Melnikov <melnikaite.melnikaite@gmail.com>
    Reviewed-by: Sergey Sergyenko <sergey.sergyenko@gmail.com>

--- app/controllers/omniauth_controller.rb
+++ app/controllers/omniauth_controller.rb
@@ -1,6 +1,7 @@
 class OmniauthController < ApplicationController
   def passthru
-    if request.referer && request.referer !~ %r{auth/\w+/callback}
+    not_redirectable_urls = [%r{auth/\w+/callback}, %r{users/sign_in}, %r{users/sign_up}]
+    if request.referer && not_redirectable_urls.all? {|url| request.referer !~ url}
       store_location request.referer
     end
     render :file => File.join(Rails.root, 'public', '404.html'), :status => 404, :layout => false
