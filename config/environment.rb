2a49d69622216d65ee57136689562c2b29a2cced
Eugene Melnikov <melnikaite.melnikaite@gmail.com>
2011-10-18 18:09:06 +0300

    Facebook and Vkontakte blocks
    Added Facebook and Vkontakte blocks to general layout.  Id of
    social groups moved to config.  Optomized process of loading
    config file.
    
    Story: 298
    Ticket: 314, 315
    Change-Id: I12005e2d04496116190819502934bd58933f1d18
    Reviewed-on: http://gerrit.altoros.corp/119
    Tested-by: Eugene Melnikov <melnikaite.melnikaite@gmail.com>
    Reviewed-by: Sergey Sergyenko <sergey.sergyenko@gmail.com>

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,9 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+# Load config for social services
+SOCIAL_CONFIG = YAML.load_file("#{Rails.root}/config/social_config.yml")[Rails.env]
+
 # Initialize the rails application
 BelarusRubyOnRails::Application.initialize!
 
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,26 +1,24 @@
-CONFIG = YAML.load_file("#{Rails.root}/config/credentials.yml")[Rails.env]
-
 Devise.setup do |config|
 
-  credentials = CONFIG['github'] || {}
+  credentials = SOCIAL_CONFIG['github'] || {}
 
   config.omniauth :github, credentials['app_id'], credentials['secret_key'],
                   :scope => 'user,public_repo'
 
-  credentials = CONFIG['facebook'] || {}
+  credentials = SOCIAL_CONFIG['facebook'] || {}
   config.omniauth :facebook, credentials['app_id'], credentials['secret_key'],
                   :scope => 'email'
 
-  credentials = CONFIG['vkontakte'] || {}
+  credentials = SOCIAL_CONFIG['vkontakte'] || {}
   config.omniauth :vkontakte, credentials['app_id'], credentials['secret_key']
 
-  credentials = CONFIG['twitter'] || {}
+  credentials = SOCIAL_CONFIG['twitter'] || {}
   config.omniauth :twitter, credentials['app_id'], credentials['secret_key']
 
   config.omniauth :google_apps, OpenID::Store::Filesystem.new('/tmp'),
                   :domain => 'gmail.com'
 
-  credentials = CONFIG['linkedin'] || {}
+  credentials = SOCIAL_CONFIG['linkedin'] || {}
   config.omniauth :linked_in, credentials['app_id'], credentials['secret_key'],
                   :site => 'https://api.linkedin.com/',
                   :authorize_path => '/uas/oauth/authorize',
