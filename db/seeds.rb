bc6a8c5415e7b647c6edf3b2c6f50ca5fc2b8947
Eugene Melnikov <melnikaite.melnikaite@gmail.com>
2011-10-20 09:41:39 +0300

    Seeds for expiriences
    Change-Id: I010567b76070e7423d3cf70ef5bfe86535684dfc
    Reviewed-on: http://gerrit.altoros.corp/125
    Tested-by: Eugene Melnikov <melnikaite.melnikaite@gmail.com>
    Reviewed-by: Denis Sheleh <den.sheleh@gmail.com>
    Reviewed-by: Sergey Sergyenko <sergey.sergyenko@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# encoding: UTF-8
+#
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +7,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+levels = if I18n.locale.to_s == 'ru'
+           ['Что такое Ruby on Rails?', 'Новичок', 'Средний', 'Эксперт']
+         else
+           ['What is Ruby on Rails?', 'Beginner', 'Intermediate', 'Expert']
+         end
+Experience.delete_all
+levels.each do |level|
+  Experience.create(:level => level)
+end
