d750ff53b108c7372e27e65a040e74a86177f9b0
Viktor Melnikov <heaven.out.there@gmail.com>
2011-10-28 14:36:38 +0300

    Localization of error messages
    Add missing localizations for form errors
    
    Change-Id: I7f9807e58b5069e52186d4a034bcf3ea5413797e
    Reviewed-on: http://gerrit.altoros.corp/153
    Tested-by: Viktor Melnikov <heaven.out.there@gmail.com>
    Reviewed-by: Sergey Sergyenko <sergey.sergyenko@gmail.com>

--- app/models/meetup.rb
+++ app/models/meetup.rb
@@ -8,7 +8,8 @@ class Meetup < ActiveRecord::Base
   validates :description, :presence => true
   validates :place, :presence => true
   validates :place, :length => {:maximum => 255}
-  validates_acceptance_of :date_and_time, :if => Proc.new { |meetup| meetup.date_and_time.past? }, :message => I18n.t('meetup.datetime_must_future')
+  validates :date_and_time, :presence => true
+  validates_acceptance_of :date_and_time, :if => Proc.new { |meetup| meetup.date_and_time && meetup.date_and_time.past? }, :message => I18n.t('meetup.datetime_must_future')
 
   scope :active, lambda { where('date_and_time > ? AND cancelled = ?', Time.new, false) }
   scope :recent, order('date_and_time')
