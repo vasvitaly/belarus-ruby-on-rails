730cf44b79ad15fa5bbd08725d0a172f3961a980
Andrey Koleshko <ka8725@gmail.com>
2012-03-27 14:02:35 +0300

    assets precompile changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ BelarusRubyOnRails::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( vkontakte_block.js twitter_block.js profile.js i18n.js i18n/translations.js i18n/settings.js )
+  config.assets.precompile += %w( vkontakte_block.js twitter_block.js profile.js ) 
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
