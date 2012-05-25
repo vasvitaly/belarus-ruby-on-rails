5599dc76e19b1f17439d29ab10618657d8d3184b
Andrey Koleshko <ka8725@gmail.com>
2012-05-25 15:27:15 +0300

    fix questionire

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,3 +1,6 @@
 class Answer < ActiveRecord::Base
+  attr_accessible :gist
   belongs_to :question
+  # TODO: fix required class in the styles.css for uncomment this line, because it is hidden now!
+  #  validates :gist, :presence => true
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,9 +1,12 @@
 class Question < ActiveRecord::Base
   RESPONSE_TYPES = %w(dropdown checkboxes text_field).freeze
-  attr_accessible :answers_attributes
+  attr_accessible :answers_attributes, :gist
   belongs_to  :meetup
   has_many    :answers
   accepts_nested_attributes_for :answers
+
+# TODO: fix required class in the styles.css for uncomment this line, because it is hidden now!
+#  validates :gist, :presence => true
 
   def self.response_type_options_for_select
     RESPONSE_TYPES.map { |r| [human_attribute_name(r), RESPONSE_TYPES.index(r)] }
