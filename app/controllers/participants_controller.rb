ae219f09e84f1885361141fb13cd743bf2c176e1
Andrey Koleshko <ka8725@gmail.com>
2012-05-25 17:25:41 +0300

    send notify on create new participant

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -11,6 +11,8 @@ class ParticipantsController < ApplicationController
   def create
     @participant = Participant.new(params[:participant].merge(:user => current_user, :meetup => @meetup))
     if @participant.save
+      Notifier.new_participant_for_meetup(@meetup, @participant).deliver
+      Notifier.new_participant_for_meetup_for_admin(@meetup, @participant).deliver
       redirect_to root_path, :notice => 'Вы зарегистрировались мероприятие успешно'
     else
       flash[:error] = 'Форма содержит ошибки'
