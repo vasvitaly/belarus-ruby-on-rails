8d6d8b504b27c92c3223bb4637b28c83703cda3c
Eugene Melnikov <melnikaite.melnikaite@gmail.com>
2012-05-29 14:03:58 +0300

    removed seconds from datetimepicker format

--- app/assets/javascripts/datepicker.js
+++ app/assets/javascripts/datepicker.js
@@ -2,6 +2,6 @@ $(document).ready(function(){
     $('.datepicker').datetimepicker({
         minDate: 0,
         dateFormat: 'yy-mm-dd',
-        timeFormat: 'hh:mm:ss'
+        timeFormat: 'hh:mm'
     })
 })
