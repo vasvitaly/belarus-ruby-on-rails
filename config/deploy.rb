d5045c6e957237b6292b7c853987b768ce346ef7
Eugene Melnikov <melnikaite.melnikaite@gmail.com>
2012-06-07 22:23:54 +0300

    perform cleanup old releases

--- config/deploy.rb
+++ config/deploy.rb
@@ -54,3 +54,5 @@ after "deploy:restart", "solr:symlink"
 after "deploy:stop",    "delayed_job:stop"
 after "deploy:start",   "delayed_job:start"
 after "deploy:restart", "delayed_job:restart"
+
+after "deploy:restart", "deploy:cleanup"
