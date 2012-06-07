9ae47cdae3539a7fb943a17725ece43d3b0ffc88
Eugene Melnikov <melnikaite.melnikaite@gmail.com>
2012-06-08 00:06:07 +0300

    temporary fix while delayed jobs doesn't work on production

--- app/controllers/admin/messages_controller.rb
+++ app/controllers/admin/messages_controller.rb
@@ -10,7 +10,8 @@ class Admin::MessagesController < ApplicationController
 
     if @message.valid?
       Profile.subscribed.filter(@message.recipient_group).includes(:user).uniq.each do |recipient|
-        Notifier.delay.broadcast_message(recipient.user.email, @message.subject, @message.body)
+        #Notifier.delay.broadcast_message(recipient.user.email, @message.subject, @message.body)
+        Notifier.broadcast_message(recipient.user.email, @message.subject, @message.body).deliver
       end
       redirect_to admin_root_url, :notice => I18n.t('admin.messages.successfully_sent')
     else
