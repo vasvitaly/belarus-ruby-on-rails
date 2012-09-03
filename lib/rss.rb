01c0eb74b8050892b271c67da19a1859210dc39e
Eugene Melnikov <melnikaite.melnikaite@gmail.com>
2012-09-03 17:08:33 +0300

    collect only latest news

--- lib/rss.rb
+++ lib/rss.rb
@@ -15,6 +15,7 @@ module RSS
         new_entries = feed_object.entries
       end
       new_entries.each do |entry|
+        next if (Date.today - entry.published.to_date).to_i != 0
         next unless entry.url
         rss_link = entry.url
         complex_rss_link = rss_link.scan(/&url=(.*?)$/)
