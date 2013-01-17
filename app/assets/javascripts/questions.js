a714019d230af79a94e02d13f8c2b8480882f4bc
Sergey Starikov <starikov.by@gmail.com>
2013-01-17 12:12:14 +0300

    fixed javascript for yes\no questions

--- app/assets/javascripts/questions.js
+++ app/assets/javascripts/questions.js
@@ -1,21 +1,18 @@
-$(document).ready(function() {
     $(".answer_choise").change(function() {
         if ($(this).find("option[value='true']").attr('selected')) {
-            addTextArea($(this).attr('name'), $(this).attr('id'), $('.answer_hint').attr('label'));
+            addTextArea($(this).attr('name'), $(this).attr('id'));
         }
         else {
-            dellTextArea($(this).attr('id'))
+            dellTextArea($(this).attr('id'));
         }
-    });
 });
 
-function addTextArea(name, id, hint) {
-    $('.answer_choise').parent().append("<br />");
-    $('.answer_choise').parent().append("<text type='text', value='"+hint+"'></text>" );
-    $('.answer_choise').parent().append("<textarea type='textarea' cols='40' rows='5' id='"+id+"_textarea' name='"+name+"' " + "'></textarea>");
+function addTextArea(name, id) {
+    $('#'+id).parent().append("<br />");
+    $('#'+id).parent().append("<textarea type='textarea' cols='40' rows='5' id='"+id+"_textarea' name='"+name+"' " + "'></textarea>");
 }
 
 function dellTextArea(id) {
-    $("#"+id+"_textarea").remove()
-    $('br').remove()
+    $("#"+id+"_textarea").remove();
+    $('br').remove();
 }
