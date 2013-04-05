09a4c4cbd955ed767869d9a9dfbfee3800673698
Eugene Melnikov <melnikaite.melnikaite@gmail.com>
2013-04-05 20:59:43 +0300

    implemented pending tests

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
 describe Answer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should allow_mass_assignment_of :gist }
+  it { should belong_to(:question) }
+  it { should have(1).error_on(:gist) }
 end
