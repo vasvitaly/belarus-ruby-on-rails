a52f822efa36f3c6ecbb38feb9b90af64bf5e26d
Yulia Oletskaya <voletsky@gmail.com>
2013-09-27 12:46:46 +0300

    tune valid icalendar format

--- spec/factories/meetups.rb
+++ spec/factories/meetups.rb
@@ -8,4 +8,11 @@ FactoryGirl.define do
     date_and_time Time.now + 1.day
     finish_date_and_time Time.now + 1.month
   end
+
+  factory :meetup_ical, :class => Meetup do
+    topic 'Topic of meetup'
+    description 'Description of meetup'
+    date_and_time 'Thu May 22 16:30:00 -0400 2008'
+    finish_date_and_time 'Thu May 22 18:30:00 -0400 2008'
+  end
 end
