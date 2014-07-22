f1fc7d38e9cd52017c15d431fd8aceb0137961fd
Eugene Melnikov <melnikaite.melnikaite@gmail.com>
2014-07-22 13:53:32 +0300

    rename user group

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,9 +9,9 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 levels = if I18n.locale.to_s == 'ru'
-           ['Что такое Ruby?', 'Новичок', 'Средний', 'Эксперт']
+           ['Что такое PaaS?', 'Новичок', 'Средний', 'Эксперт']
          else
-           ['What is Ruby?', 'Beginner', 'Intermediate', 'Expert']
+           ['What is PaaS?', 'Beginner', 'Intermediate', 'Expert']
          end
 Experience.delete_all
 levels.each do |level|
@@ -20,26 +20,26 @@ end
 
 twitter_block_settings = if I18n.locale.to_s == 'ru'
                           {:title => 'Что пишут о',
-                          :subject => 'Belarus Ruby',
+                          :subject => 'Belarus PaaS',
                           :footer_text => 'Присоединиться к обсуждению'}
                         else
                           {:title => 'What is written about',
-                          :subject => 'Belarus Ruby',
+                          :subject => 'Belarus PaaS',
                           :footer_text => 'Join the conversation'}
                         end
 TwitterBlock.delete_all
 TwitterBlock.create(
   :title => twitter_block_settings[:title],
   :subject => twitter_block_settings[:subject],
-  :search => '#hacby',
+  :search => '#paas',
   :footer_text => twitter_block_settings[:footer_text]
 )
 
 StaticPage.delete_all
 content_about = if I18n.locale.to_s == 'ru'
-                  '<p>Белорусская Ruby User Group - сообщество Ruby-девелоперов в Беларуси, созданное для обмена идеями и опытом. Мы также заинтересованы в развитии Ruby в нашей стране и помогаем друг другу построить успешную IT-карьеру.</p><p>Если Вы тоже желаете участвовать в развитии Belarus Ruby User Group, присылайте свои идеи на адрес:</p><p><a href="mailto:info@brug.by">info@brug.by</a></p>'
+                  '<p>Белорусская PaaS User Group - сообщество PaaS-девелоперов в Беларуси, созданное для обмена идеями и опытом. Мы также заинтересованы в развитии PaaS в нашей стране и помогаем друг другу построить успешную IT-карьеру.</p><p>Если Вы тоже желаете участвовать в развитии Belarus PaaS User Group, присылайте свои идеи на адрес:</p><p><a href="mailto:info@paaspro.by">info@paaspro.by</a></p>'
                 else
-                  '<p>Belarussian Ruby User Group is community of Ruby-developers in Belarus, was found for exchenging ideas and expirience. We are also interested in developing Ruby in our country and help each other to build successfull IT-career.</p><p>If you would like take part in developing Belarus Ruby User Group, please send your ideas to: <a href="mailto:info@brug.by">info@brug.by</a></p>'
+                  '<p>Belarussian PaaS User Group is community of PaaS-developers in Belarus, was found for exchenging ideas and expirience. We are also interested in developing PaaS in our country and help each other to build successfull IT-career.</p><p>If you would like take part in developing Belarus PaaS User Group, please send your ideas to: <a href="mailto:info@paaspro.by">info@paaspro.by</a></p>'
                 end
 
 title_about = if I18n.locale.to_s == 'ru'
@@ -59,5 +59,5 @@ StaticPage.create(:title => title_about, :permalink => 'about', :content => cont
 StaticPage.create(:title => title_friends, :permalink => 'friends', :content => content_friends)
 
 AggregatorConfiguration.create(
-  :source => 'http://news.google.com/news?cf=all&ned=ru_ru&hl=ru&q=%22ruby+on+rails%22+OR+%22Ruby-on-RAILS%22+OR+%22ruby+development%22+OR+%22rails+development%22+OR+%22ruby+developers%22+OR+%22rails+developers%22&as_qdr=d&as_drrb=q&cf=all&scoring=n&output=rss'
+  :source => 'http://news.google.com/news?cf=all&ned=ru_ru&hl=ru&q=paas&as_qdr=d&as_drrb=q&cf=all&scoring=n&output=rss'
 )
