276594ddf0a8eeb076766f41e88ad403d03ec7c9
Eugene Melnikov <melnikaite.melnikaite@gmail.com>
2014-07-24 12:23:36 +0300

    changed host for urls in emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ BelarusRubyOnRails::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => 'brug.by' }
+  config.action_mailer.default_url_options = { :host => 'paaspro.by' }
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
