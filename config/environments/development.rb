c8332e05c6b0749cfb7b768029e22b8b3d682837
Eugene Melnikov <melnikaite.melnikaite@gmail.com>
2014-10-29 18:08:15 +0300

    rename emails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,7 +26,7 @@ BelarusRubyOnRails::Application.configure do
 #  config.action_mailer.delivery_method = :sendmail
 #  config.action_mailer.sendmail_settings = { :arguments => '-i' }
   config.action_mailer.default :charset => 'utf-8',
-                               :from => 'no-reply@brug.by'
+                               :from => 'no-reply@paaspro.by'
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
