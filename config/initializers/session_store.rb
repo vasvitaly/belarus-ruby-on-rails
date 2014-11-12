e7b24d4f4bfad59d0c6c7bf429cf3fdd8367e491
Vitali Vasileuski <v5778844@gmail.com>
2014-11-12 16:54:24 +0300

    Move session key to site config

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-BelarusRubyOnRails::Application.config.session_store :cookie_store, key: '_BelarusRubyOnRails_session'
+BelarusRubyOnRails::Application.config.session_store :cookie_store, key: SITE_CONFIG['cookie_store']['key']
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
